2951cd3587ce3d01956f686c4c5a19e58ff78328
Mariano <tercerpoder59@hotmail.com>
2022-07-10 21:56:08 -0300

    portfolio updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,9 +29,9 @@ const NavBar = () => {
  ]
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+    <div className="flex justify-between items-center w-full h-16 px-4 text-white bg-black fixed">
       <div>
-        <h1 className="text-5xl font-signature ml-2">Mariano Ferrer</h1>
+        <h1 className="text-4xl font-signature ml-2">Mariano Ferrer</h1>
       </div>
 
       <ul className=" text-2xl  hidden md:flex">
